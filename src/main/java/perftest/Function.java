8d27f56df125607c39ebeac892c43b890d9a0adb
Shreyas Gopalakrishna <shreyasg@microsoft.com>
2022-09-29 14:40:11 -0500

    Using transaction to read

--- src/main/java/perftest/Function.java
+++ src/main/java/perftest/Function.java
@@ -8,30 +8,17 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-
-
-
-import java.util.Optional;
-
+import com.mongodb.*;
+import com.mongodb.client.*;
 import org.bson.Document;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactor.client.MongoReactorKt;
-import com.mongodb.reactor.client.ReactorMongoCollection;
 
-
-import static com.mongodb.client.model.Filters.*;
+import java.util.Objects;
+import java.util.Optional;
 
 public class Function {
 
-    // static MongoCollection<Document> collection;
-    static ReactorMongoCollection<Document> collection;
-    static {
-        MongoClient mongoClient = MongoClients.create(new ConnectionString(System.getenv("MONGO_URI")));
-        collection =  MongoReactorKt.toReactor(mongoClient.getDatabase(System.getenv("MONGO_DB")).getCollection(System.getenv("MONGO_COLLECTION")));
-    }
+    final MongoClient client = MongoClients.create(new ConnectionString(System.getenv("MONGO_URI")));
 
     static String  field1 = System.getenv("MONGO_FIELD_1");
     static String  value1 = System.getenv("MONGO_VALUE_1");
@@ -44,10 +31,54 @@ public class Function {
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        
+        long startTime = System.currentTimeMillis();
+        long endTime, duration;
 
-        Document t = collection.find(eq(field1, value1)).first().block();
+        ClientSession session = getNewClientSession();
+        String result = withTransaction(session, getTransactionBody(session), getTransactionOptions());
 
-        return request.createResponseBuilder(HttpStatus.OK).body(t.toString()).build();
+        client.close();
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        context.getLogger().info("Time taken for the function " + duration);
+        if(Objects.isNull(result)){
+            return request.createResponseBuilder(HttpStatus.OK).body("Returning null value!").build();
+        }
+        return request.createResponseBuilder(HttpStatus.OK).body(result).build();
     }
+
+    private TransactionBody getTransactionBody(ClientSession session) {
+        TransactionBody txnBody = new TransactionBody<String>() {
+            public String execute() {
+                MongoCollection<Document> coll1 = client.getDatabase(System.getenv("MONGO_DB")).getCollection(System.getenv("MONGO_COLLECTION"));
+                Document doc = coll1.find().first();
+                return doc.get("_id").toString();
+            }
+        };
+        return txnBody;
+    }
+
+    public ClientSession getNewClientSession() {
+        return client.startSession();
+    }
+
+    public static TransactionOptions getTransactionOptions() {
+        return TransactionOptions.builder()
+                .readPreference(ReadPreference.primary())
+                .readConcern(ReadConcern.LOCAL)
+                .writeConcern(WriteConcern.MAJORITY)
+                .build();
+    }
+
+    public String withTransaction(ClientSession session, TransactionBody transactionBody, TransactionOptions transactionOptions) {
+        try {
+            return session.withTransaction(transactionBody, transactionOptions).toString();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+
 }
